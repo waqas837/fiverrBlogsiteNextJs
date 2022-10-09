4ebe9f6499426fac2bee706b5da418abfb266a45
waqas837 <mwaqas3030@gmail.com>
2022-10-10 00:32:03 +0500

    make it better

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,87 +1,117 @@
-import Format from '../../layout/format'
-import Author from '../../components/_child/author'
-import Image from 'next/image'
-import Ralated from '../../components/_child/ralated'
-import getPost from '../../lib/helper'
-import useFetcher from '../../lib/useFetcher';
-import Spinner from '../../components/_child/spinner'
-import ErrorComponent from '../../components/_child/error'
-import { useRouter } from 'next/router'
-import { SWRConfig } from 'swr'
+import Format from "../../layout/format";
+import Author from "../../components/_child/author";
+import Image from "next/image";
+import Ralated from "../../components/_child/ralated";
+import getPost from "../../lib/helper";
+import useFetcher from "../../lib/useFetcher";
+import Spinner from "../../components/_child/spinner";
+import ErrorComponent from "../../components/_child/error";
+import { useRouter } from "next/router";
+import { SWRConfig } from "swr";
 
-export default function Page({ fallback }){
+export default function Page({ title, img, subtitle, description, author }) {
+  // const router = useRouter()
+  // const { postId } = router.query;
+  // const { data, isLoading, isError } = useFetcher(`api/posts/${postId}`)
 
-    const router = useRouter()
-    const { postId } = router.query;
-    const { data, isLoading, isError } = useFetcher(`api/posts/${postId}`)
+  // if(isLoading) return <Spinner></Spinner>
+  // if(isError) return <ErrorComponent></ErrorComponent>
 
-    if(isLoading) return <Spinner></Spinner>
-    if(isError) return <ErrorComponent></ErrorComponent>
+  // return (
+  //     <SWRConfig value={ { fallback }}>
+  //         <Article {...data}></Article>
+  //     </SWRConfig>
+  // )
+  return (
+    <Format>
+      <section className="container mx-auto md:px-2 py-16 w-1/2">
+        <div className="flex justify-center">
+          {author ? <Author {...author}></Author> : <></>}
+        </div>
 
-    return (
-        <SWRConfig value={ { fallback }}>
-            <Article {...data}></Article>
-        </SWRConfig>
-    )
+        <div className="post py-10">
+          <h1 className="font-bold text-4xl text-center pb-5">
+            {title || "No Title"}
+          </h1>
 
+          <p className="text-gray-500 text-xl text-center">
+            {subtitle || "No Title"}
+          </p>
+
+          <div className="py-10">
+            <Image src={img || "/"} width={900} height={600}></Image>
+          </div>
+
+          <div className="content text-gray-600 text-lg flex flex-col gap-4">
+            {description || "No Description"}
+          </div>
+        </div>
+
+        <Ralated></Ralated>
+      </section>
+    </Format>
+  );
 }
 
-function Article({ title, img, subtitle, description, author }){
+function Article({ title, img, subtitle, description, author }) {
+  return (
+    <Format>
+      <section className="container mx-auto md:px-2 py-16 w-1/2">
+        <div className="flex justify-center">
+          {author ? <Author {...author}></Author> : <></>}
+        </div>
 
-    return (
-        <Format>
-            <section className='container mx-auto md:px-2 py-16 w-1/2'>
-                <div className='flex justify-center'>
-                    { author ? <Author {...author}></Author> : <></>}
-                </div>
+        <div className="post py-10">
+          <h1 className="font-bold text-4xl text-center pb-5">
+            {title || "No Title"}
+          </h1>
 
-                <div className="post py-10">
-                    <h1 className='font-bold text-4xl text-center pb-5'>{title || "No Title"}</h1>
+          <p className="text-gray-500 text-xl text-center">
+            {subtitle || "No Title"}
+          </p>
 
-                    <p className='text-gray-500 text-xl text-center'>{subtitle || "No Title"}</p>
+          <div className="py-10">
+            <Image src={img || "/"} width={900} height={600}></Image>
+          </div>
 
-                    <div className="py-10">
-                        <Image src={img || "/"} width={900} height={600}></Image>
-                    </div>
+          <div className="content text-gray-600 text-lg flex flex-col gap-4">
+            {description || "No Description"}
+          </div>
+        </div>
 
-                    <div className="content text-gray-600 text-lg flex flex-col gap-4">
-                        {description || "No Description"}
-                    </div>
-
-                </div>  
-
-                <Ralated></Ralated>
-            </section>
-        </Format>
-    )
+        <Ralated></Ralated>
+      </section>
+    </Format>
+  );
 }
 
+export async function getServerSideProps({ params }) {
+  const posts = await getPost(params.postId);
 
-export async function getStaticProps( { params } ){
-    const posts = await getPost(params.postId)
-
-    return {
-       props : {
-            fallback : {
-                '/api/posts' : posts
-            }
-       }
-    }
+  return {
+    props: {
+      title: posts.title,
+      img: posts.img,
+      subtitle: posts.subtitle,
+      description: posts.description,
+      author: posts.author,
+    },
+  };
 }
 
-export async function getStaticPaths(){
-    const posts = await getPost();
-    const paths = posts.map(value => {
-        return {
-            params : {
-                postId : value.id.toString()
-            }
-        }
-    })
+// export async function getStaticPaths(){
+//     const posts = await getPost();
+//     const paths = posts.map(value => {
+//         return {
+//             params : {
+//                 postId : value.id.toString()
+//             }
+//         }
+//     })
 
-    return {
-        paths,
-        fallback : false
-    }
+//     return {
+//         paths,
+//         fallback : false
+//     }
 
-}
+// }
